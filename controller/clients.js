57a7acee44a7960ddbceaebacb495772eb9a3141
Sara Chaieb <sara.chaieb@epita.fr>
2020-11-19 17:05:03 +0100

    refacto(client): html

--- controller/clients.js
+++ controller/clients.js
@@ -7,10 +7,22 @@ module.exports = function(app) {
 }
 
 controller.clients = async function(req, res, next){
-    var result = "Clients : ";
+    var result = [];
     
     await axios.get(process.env.URL_RELATION_CLIENT + '/client-account-list').then(function (response) {
-        result = result.concat(JSON.stringify(response.data));
+        response.data.map((client) => {
+            result.push({
+                id: client.id,
+                account: client.account,
+                lastname: client["last-name"],
+                firstname: client["first-name"],
+                dob: client["date-of-birth"],
+                civility: client.civility,
+                customerLoyalty: client["customer-loyalty"],
+                credit: client.credit,
+                payment: client.payment
+            })
+        })
     }).catch((err) => {
         result = result.concat(err.data);
     });
