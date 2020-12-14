2802a4cfaf6e0fcf9a09bd3e6423dc1cc3e5fcd3
Nathan Tournant <nathan.tournant@epita.fr>
2020-12-14 12:23:14 +0100

    feat(ci): added test

--- models/setup.js
+++ models/setup.js
@@ -2,12 +2,12 @@ const logger = require("../logs");
 const { Sequelize, DataTypes } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.URSI_DB_NAME,
+  process.env.URSI_DB_USER,
+  process.env.URSI_DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
+    host: process.env.URSI_DB_HOST,
+    port: Number(process.env.URSI_DB_PORT),
     logging: (msg) => logger.verbose(msg),
     dialectOptions: {
       timezone: "Etc/GMT0",
